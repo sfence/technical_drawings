55bd3c60835fcbd31e2871861ff573bcd84c08be
SFENCE <sfence.software@gmail.com>
2021-09-27 19:47:43 +0200

    Add base of drawing copmare functions.

--- functions.lua
+++ functions.lua
@@ -80,3 +80,155 @@ function technical_drawings.tool_on_use(itemstack, user, pointed_thing)
   return itemstack;
 end
 
+
+-- comparing drawings
+local function drawings_diff_core(A, B)
+  local ratio = A.res/B.res;
+  local check = math.modf(ratio);
+  if check~=ratio then
+    -- only integer multiples resolutions can be compared
+    --return A.res*B.res;
+    --return 1;
+  end
+  
+  -- palette conjuction table
+  local conjuction = {};
+  
+  -- same size drawing
+  --   statistic comparation for palette conjuction point to point
+  
+  -- different size of drawing
+  --   use method point to multiple point
+  --   should be done in good way, to prevent background to be identified as line. Is it possible?
+  
+  -- what about shitch A and B? (multicolored drawing from player match to ideal, no match ideal to player)
+  for y=1,A.res do
+    local lineA = A.grid[y];
+    for x=1,A.res do
+      -- use next cycle for different resolution of drawing
+      local pA = lineA[x];
+      local pB = B.grid[math.ceil(y/ratio)][math.ceil(x/ratio)];
+      if (pA~=0) and (pB~=0) then -- ignore DO NOT MATTER palette indexes
+        local conjA = conjuction[pA];
+        if not conjA then
+          conjA = {
+              conj = {},
+            };
+          conjuction[pA] = conjA;
+        end
+        --[[
+        if conjA.conj[pB] then
+          conjA.conj[pB] = conjA.conj[pB] + 1;
+        else
+          conjA.conj[pB] = 1;
+        end--]]
+        conjA.conj[pB] = (conjA.conj[pB] or 0) + 1;
+      end
+    end
+  end
+  
+  --print(dump(conjuction))
+  -- I have conjuction, so select the best matches
+  local convertion = {};
+  
+  for orig_palette,conj in pairs(conjuction) do
+    local vmax = 0;
+    local pmax = 0;
+    for palette,count in pairs(conj.conj) do
+      if (count>vmax) then
+        vmax = count;
+        pmax = palette;
+      end
+    end
+    convertion[orig_palette] = pmax;
+  end
+  --print(dump(convertion))
+  
+  local diff = 0;
+  
+  for y=1,A.res do
+    local lineA = A.grid[y];
+    for x=1,A.res do
+      local pB = B.grid[math.ceil(y/ratio)][math.ceil(x/ratio)];
+      if (lineA[x]~=pB) then
+        diff = diff + 1;
+      end
+    end
+  end
+  
+  return diff/(A.res*A.res);
+end
+
+function technical_drawings.drawings_diff(A, B)
+  if (A.res>=B.res) then
+    return drawings_diff_core(A, B);
+  else
+    return drawings_diff_core(B, A);
+  end
+end
+
+local function HexToRGB(hex)
+  color = tonumber(hex, 16);
+  return {
+      r = (color>>16)&255,
+      g = (color>>8)&255,
+      b = color&255,
+    }
+end
+local function RGBToHex(color)
+  return string.format("%02x%02x%02x", color.r, color.g, color.b);
+end
+local function DiffRGB(color1, color2)
+  return math.abs(color1.r-color2.r)+math.abs(color1.g-color2.g)+math.abs(color1.b-color2.b);
+end
+
+function technical_drawings.drawing_to_palette(drawing, max_colors_diff)
+  local palette = {};
+  
+  for _,line_data in pairs(drawing.grid) do
+    for _,point_data in pairs(line_data) do
+      local new_color = true;
+      for _,palette_color in pairs(palette) do
+        if (DiffRGB(palette_color.center_color, HexToRGB(point_data))<=max_colors_diff) then
+          new_color = false;
+          if palette_color.colors[point_data] then
+            palette_color.colors[point_data] = palette_color.colors[point_data] + 1;
+          else
+            palette_color.colors[point_data] = 1;
+          end
+          break;
+        end
+      end
+      if new_color then
+        table.insert(palette, {
+            center_color = HexToRGB(point_data),
+            colors = {[point_data] = 1}
+          });
+      end
+    end
+  end
+  
+  --print(dump(palette))
+  
+  -- reduce drawing
+  local paletted = {};
+  for _,line_data in pairs(drawing.grid) do
+    local new_line = {};
+    for _,point_data in pairs(line_data) do
+      for palette_index,palette_color in pairs(palette) do
+        if (DiffRGB(palette_color.center_color, HexToRGB(point_data))<=max_colors_diff) then
+          table.insert(new_line, palette_index);
+          break;
+        end
+      end
+    end
+    table.insert(paletted, new_line);
+  end
+  
+  return {
+      res = drawing.res,
+      grid = paletted,
+    }
+end
+
+
